e1c4b4480fa8206a7365e762ef29d81d926074e5
oluwapoloreoyeniji <oluwapolorealexander@gmail.com>
2024-01-11 05:16:01 +0100

    defines total attribute, fixes compute_avg_faster

--- listavg.py
+++ listavg.py
@@ -1,10 +1,13 @@
 class ListAverage:
     def __init__(self, lst):
         self.lst = lst.copy()
-        self.length = len(lst)
+        self.total = 0
+        for i in self.lst:
+            self.total += i
 
     def add(self, num):
         self.lst.append(num)
+        self.total += num
 
     def compute_avg(self):
         total = 0
@@ -13,8 +16,7 @@ class ListAverage:
         return total / len(self.lst)
 
     def compute_avg_faster(self):
-        # implement this method
-        return 
+        return self.total / len(self.lst)
     
 ### getting the length once will def reduce the time as no more time/memory is spent on doing that
 
